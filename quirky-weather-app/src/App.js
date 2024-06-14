a71b50bba65775a3580ecffdb638b1b2016612c5
Odilon Irambona <odilonirambona@Irembo-170-OIrambona.local>
2024-06-14 17:57:15 +0200

    updated the design

--- quirky-weather-app/src/App.js
+++ quirky-weather-app/src/App.js
@@ -13,263 +13,289 @@ import ErrorBox from './components/Reusable/ErrorBox';
 import { ALL_DESCRIPTIONS } from './utilities/DateConstants';
 import PersonIcon from '@mui/icons-material/Person';
 import {
-  getTodayForecastWeather,
-  getWeekForecastWeather,
+    getTodayForecastWeather,
+    getWeekForecastWeather,
 } from './utilities/DataUtils';
+import './App.css';
 
 function App() {
-  const [todayWeather, setTodayWeather] = useState(null);
-  const [todayForecast, setTodayForecast] = useState([]);
-  const [weekForecast, setWeekForecast] = useState(null);
-  const [comparisonWeather, setComparisonWeather] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isActive] = useState(false);
-  const [error, setError] = useState(false);
+    const [todayWeather, setTodayWeather] = useState(null);
+    const [todayForecast, setTodayForecast] = useState([]);
+    const [weekForecast, setWeekForecast] = useState(null);
+    const [comparisonWeather, setComparisonWeather] = useState(null);
+    const [comparisonTodayForecast, setComparisonTodayForecast] = useState([]); // New state for comparison today forecast
+    const [comparisonWeekForecast, setComparisonWeekForecast] = useState(null); // New state for comparison weekly forecast
+    const [isLoading, setIsLoading] = useState(false);
+    const [isActive] = useState(false);
+    const [error, setError] = useState(false);
+    const [showRain, setShowRain] = useState(false);
 
-  const searchChangeHandler = async (enteredData) => {
-    const [latitude, longitude] = enteredData.value.split(' ');
+    const searchChangeHandler = async (enteredData) => {
+        const [latitude, longitude] = enteredData.value.split(' ');
 
-    setIsLoading(true);
+        setIsLoading(true);
+        setShowRain(true);
 
-    const currentDate = transformDateFormat();
-    const date = new Date();
-    let dt_now = Math.floor(date.getTime() / 1000);
+        const currentDate = transformDateFormat();
+        const date = new Date();
+        let dt_now = Math.floor(date.getTime() / 1000);
 
-    try {
-      const [todayWeatherResponse, weekForecastResponse] =
-        await fetchWeatherData(latitude, longitude);
-      const all_today_forecasts_list = getTodayForecastWeather(
-        weekForecastResponse,
-        currentDate,
-        dt_now
-      );
+        try {
+            const [todayWeatherResponse, weekForecastResponse] =
+                await fetchWeatherData(latitude, longitude);
+            const all_today_forecasts_list = getTodayForecastWeather(
+                weekForecastResponse,
+                currentDate,
+                dt_now
+            );
 
-      const all_week_forecasts_list = getWeekForecastWeather(
-        weekForecastResponse,
-        ALL_DESCRIPTIONS
-      );
+            const all_week_forecasts_list = getWeekForecastWeather(
+                weekForecastResponse,
+                ALL_DESCRIPTIONS
+            );
 
-      setTodayForecast([...all_today_forecasts_list]);
-      setTodayWeather({ city: enteredData.label, ...todayWeatherResponse });
-      setWeekForecast({
-        city: enteredData.label,
-        list: all_week_forecasts_list,
-      });
-    } catch (error) {
-      setError(true);
-    }
+            setTodayForecast([...all_today_forecasts_list]);
+            setTodayWeather({ city: enteredData.label, ...todayWeatherResponse });
+            setWeekForecast({
+                city: enteredData.label,
+                list: all_week_forecasts_list,
+            });
 
-    setIsLoading(false);
-  };
+            // Hide the rain animation after a short delay
+            setTimeout(() => setShowRain(false), 1000); // Adjust the delay as needed
+        } catch (error) {
+            setError(true);
+        }
 
-  const comparisonSearchChangeHandler = async (enteredData) => {
-    const [latitude, longitude] = enteredData.value.split(' ');
+        setIsLoading(false);
+    };
 
-    setIsLoading(true);
+    const comparisonSearchChangeHandler = async (enteredData) => {
+        const [latitude, longitude] = enteredData.value.split(' ');
 
-    const currentDate = transformDateFormat();
-    const date = new Date();
-    let dt_now = Math.floor(date.getTime() / 1000);
+        setIsLoading(true);
+        setShowRain(true);
 
-    try {
-      const [todayWeatherResponse, weekForecastResponse] =
-        await fetchWeatherData(latitude, longitude);
-      const all_today_forecasts_list = getTodayForecastWeather(
-        weekForecastResponse,
-        currentDate,
-        dt_now
-      );
+        const currentDate = transformDateFormat();
+        const date = new Date();
+        let dt_now = Math.floor(date.getTime() / 1000);
 
-      const all_week_forecasts_list = getWeekForecastWeather(
-        weekForecastResponse,
-        ALL_DESCRIPTIONS
-      );
+        try {
+            const [todayWeatherResponse, weekForecastResponse] =
+                await fetchWeatherData(latitude, longitude);
+            const all_today_forecasts_list = getTodayForecastWeather(
+                weekForecastResponse,
+                currentDate,
+                dt_now
+            );
 
-      setComparisonWeather({ city: enteredData.label, ...todayWeatherResponse });
-    } catch (error) {
-      setError(true);
-    }
+            const all_week_forecasts_list = getWeekForecastWeather(
+                weekForecastResponse,
+                ALL_DESCRIPTIONS
+            );
 
-    setIsLoading(false);
-  };
-  
+            setComparisonWeather({ city: enteredData.label, ...todayWeatherResponse });
+            setComparisonTodayForecast([...all_today_forecasts_list]);
+            setComparisonWeekForecast({
+                city: enteredData.label,
+                list: all_week_forecasts_list,
+            });
 
-  let appContent = (
-    <Box
-      xs={12}
-      display="flex"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{
-        width: '100%',
-        minHeight: '500px',
-      }}
-    >
-      
-      <SvgIcon
-  component={SplashIcon}
-  inheritViewBox
-  sx={{
-    fontSize: { xs: '100px', sm: '120px', md: '140px' },
-    strokeDashoffset: '2670.019775390625px',
-    strokeDasharray: '2670.019775390625px',
-    fill: 'transparent',
-    transition: 'stroke-dashoffset 1s cubic-bezier(0.47, 0, 0.745, 0.715) 0s, fill 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) 0.8s',
-  }}
-  className={isActive ? 'active' : ''}
-/>
+            // Hide the rain animation after a short delay
+            setTimeout(() => setShowRain(false), 1000); // Adjust the delay as needed
+        } catch (error) {
+            setError(true);
+        }
 
+        setIsLoading(false);
+    };
 
-      <Typography
-        variant="h4"
-        component="h4"
-        sx={{
-          fontSize: { xs: '12px', sm: '14px' },
-          color: 'rgba(255,255,255, .85)',
-          fontFamily: 'Poppins',
-          textAlign: 'center',
-          margin: '2rem 0',
-          maxWidth: '80%',
-          lineHeight: '22px',
-        }}
-      >
-        Explore current weather data and 6-day forecast of more than 2
-        cities!
-      </Typography>
-      {/* <Box
-  display="flex"
-  flex-direction= 'row'
-  justify-content= 'space-between'
-  width='80%'
-  height='2rem'
->
-  <Image1 />
-  <Image2 /> 
- </Box> */}
-    </Box>
-  );
-
-
-  if (todayWeather && todayForecast && weekForecast) {
-    appContent = (
-      <React.Fragment>
-        <Grid item xs={12} md={todayWeather ? 6 : 12}>
-          <Grid item xs={12}>
-            <TodayWeather data={todayWeather} forecastList={todayForecast} />
-          </Grid>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <WeeklyForecast data={weekForecast} />
-        </Grid>
-      </React.Fragment>
-    );
-  }
-
-  if (error) {
-    appContent = (
-      <ErrorBox
-        margin="3rem auto"
-        flex="inherit"
-        errorMessage="Something went wrong"
-      />
-    );
-  }
-
-  if (isLoading) {
-    appContent = (
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '100%',
-          minHeight: '500px',
-        }}
-      >
-        <LoadingBox value="1">
-          <Typography
-            variant="h3"
-            component="h3"
-            sx={{
-              fontSize: { xs: '10px', sm: '12px' },
-              color: 'rgba(255, 255, 255, .8)',
-              lineHeight: 1,
-              fontFamily: 'Poppins',
-            }}
-          >
-            Loading...
-          </Typography>
-        </LoadingBox>
-      </Box>
-    );
-  }
-
-  return (
-    <Container
-      sx={{
-        maxWidth: { xs: '95%', sm: '80%', md: '1100px' },
-        width: '100%',
-        height: '100%',
-        margin: '0 auto',
-        padding: '1rem 0 3rem',
-        marginBottom: '1rem',
-        borderRadius: {
-          xs: 'none',
-          sm: '0 0 1rem 1rem',
-        },
-        boxShadow: {
-          xs: 'none',
-          sm: 'rgba(0,0,0, 0.5) 0px 10px 15px -3px, rgba(0,0,0, 0.5) 0px 4px 6px -2px',
-        },
-      }}
-    >
-      <Grid container columnSpacing={2}>
-        <Grid item xs={12}>
-          <Box
+    let appContent = (
+        <Box
+            xs={20}
             display="flex"
-            justifyContent="space-between"
+            flexDirection="column"
             alignItems="center"
+            justifyContent="center"
             sx={{
-              width: '100%',
-              marginBottom: '1rem',
+                width: '100%',
+                minHeight: '500px',
             }}
-          >
-            <Box
-              component="img"
-              sx={{
-                height: { xs: '16px', sm: '22px', md: '26px' },
-                width: 'auto',
-              }}
-              alt="logo"
-              src={Logo}
+        >
+            <SvgIcon
+                component={SplashIcon}
+                inheritViewBox
+                className="reveal-icon"
+                sx={{padding: '1rem', fontSize: { xs: '100px', sm: '120px', md: '140px' }}}
             />
-
-            <UTCDatetime />
-            <Link
-              href="https://github.com/Gatete-Bruno"
-              target="_blank"
-              underline="none"
-              sx={{ display: 'flex' }}
-            >
-              <PersonIcon
+            <Typography
+                variant="h4"
+                component="h4"
+                className="reveal-text"
                 sx={{
-                  fontSize: { xs: '20px', sm: '22px', md: '26px' },
-                  color: 'white',
-                  '&:hover': { color: '#2d95bd' },
+                    fontSize: { xs: '20px', sm: '22px' },
+                    color: 'rgba(255,255,255, .85)',
+                    fontFamily: 'Poppins',
+                    textAlign: 'center',
+                    margin: '2rem 0',
+                    maxWidth: '80%',
+                    lineHeight: '50px',
                 }}
-              />
-            </Link>
-          </Box>
-          <Search onSearchChange={searchChangeHandler} />
-          <Search onSearchChange={comparisonSearchChangeHandler} />
-        </Grid>
-        {appContent}
-      </Grid>
-    </Container>
-  );
+            >
+
+                Welcome to the Quirky Weather App!
+                <br />
+                Search for the weather and forecast the beautiful clouds
+                <br />
+                😊 🌤️ 🌧️ 🌩️ 🌪️
+            </Typography>
+        </Box>
+    );
+
+    if (todayWeather && comparisonWeather) {
+        appContent = (
+            <React.Fragment>
+                <Grid item xs={12} md={6}>
+                    <TodayWeather data={todayWeather} forecastList={todayForecast} />
+                    <WeeklyForecast data={weekForecast} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <TodayWeather data={comparisonWeather} forecastList={comparisonTodayForecast} />
+                    <WeeklyForecast data={comparisonWeekForecast} />
+                </Grid>
+            </React.Fragment>
+        );
+    } else if (todayWeather && todayForecast && weekForecast) {
+        appContent = (
+            <React.Fragment>
+                <Grid item xs={12} md={6}>
+                    <TodayWeather data={todayWeather} forecastList={todayForecast} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <WeeklyForecast data={weekForecast} />
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
+    if (error) {
+        appContent = (
+            <ErrorBox
+                margin="3rem auto"
+                flex="inherit"
+                errorMessage="Something went wrong"
+            />
+        );
+    }
+
+    if (isLoading) {
+        appContent = (
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '100%',
+                    minHeight: '500px',
+                }}
+            >
+                <LoadingBox value="1">
+                    <Typography
+                        variant="h3"
+                        component="h3"
+                        sx={{
+                            fontSize: { xs: '10px', sm: '12px' },
+                            color: 'rgba(255, 255, 255, .8)',
+                            lineHeight: 1,
+                            fontFamily: 'Poppins',
+                        }}
+                    >
+                        Loading...
+                    </Typography>
+                </LoadingBox>
+            </Box>
+        );
+    }
+
+    return (
+        <Container
+            sx={{
+                marginTop: '2rem',
+                backdropFilter: 'blur(10px)',
+                maxWidth: { xs: '95%', sm: '80%', md: '1100px' },
+                width: '100%',
+                height: '100%',
+                margin: '0 auto',
+                padding: '2rem 0 5rem',
+                marginBottom: '1rem',
+                borderRadius: {
+                    xs: 'none',
+                    sm: '0 0 1rem 1rem',
+                },
+
+
+            }}
+        >
+            <Grid container columnSpacing={2}>
+                <Grid item xs={12}>
+                    <Box
+                        display="flex"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        sx={{
+                            width: '100%',
+                            marginBottom: '1rem',
+                        }}
+                    >
+                        <Box
+                            component="img"
+                            sx={{
+                                height: { xs: '30px', sm: '40px', md: '48px' },
+                                width: 'auto',
+                            }}
+                            alt="logo"
+                            src={Logo}
+                        />
+                        <UTCDatetime />
+                        <Link
+                            href="https://github.com/Gatete-Bruno"
+                            target="_blank"
+                            underline="none"
+                            sx={{ display: 'flex' }}
+                        >
+                            <PersonIcon
+                                sx={{
+                                    fontSize: { xs: '20px', sm: '22px', md: '26px' },
+                                    color: 'white',
+                                    '&:hover': { color: '#2d95bd' },
+                                }}
+                            />
+                        </Link>
+                    </Box>
+                    <Box
+                        display="flex"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        sx={{
+                            width: '100%',
+                            marginBottom: '1rem',
+                        }}
+                    >
+                        <Search onSearchChange={searchChangeHandler} sx={{ width: '45%' }} />
+                        <Search onSearchChange={comparisonSearchChangeHandler} sx={{ width: '45%' }} />
+                    </Box>
+                </Grid>
+                {appContent}
+                {showRain && (
+                    <Box className="rain">
+                        {[...Array(50)].map((_, i) => (
+                            <Box key={i} className="drop" />
+                        ))}
+                    </Box>
+                )}
+            </Grid>
+        </Container>
+    );
 }
-
-
 
 export default App;
--- quirky-weather-app/src/components/Search/Search.js
+++ quirky-weather-app/src/components/Search/Search.js
@@ -8,8 +8,21 @@ const Search = ({ onSearchChange }) => {
   const loadOptions = async (inputValue) => {
     const citiesList = await fetchCities(inputValue);
 
+    // Create a new Set to store unique city labels
+    const uniqueCities = new Set();
+
+    // Filter out duplicates
+    const uniqueCityList = citiesList.data.filter((city) => {
+      const label = `${city.name}, ${city.countryCode}`;
+      if (!uniqueCities.has(label)) {
+        uniqueCities.add(label);
+        return true;
+      }
+      return false;
+    });
+
     return {
-      options: citiesList.data.map((city) => {
+      options: uniqueCityList.map((city) => {
         return {
           value: `${city.latitude} ${city.longitude}`,
           label: `${city.name}, ${city.countryCode}`,
